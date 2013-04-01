5020e667bafcfc0b79d6a4f735b7888dffc8906f
Erik Clarke <ecl@mail.med.upenn.edu>
2013-04-01 15:42:34 -0400

    identifies singletons across replicates

--- preliminary_data_processing.R
+++ preliminary_data_processing.R
@@ -11,6 +11,7 @@ files <-  list.files(path = path, pattern = glob2rx("*.adap.txt"))
 ##----------------------------------------------------------------------------
 createMetadata <- function(string) {
   r <- substr(string, 9, 9)
+  # currently cannot handle replicates past "c". if more, make better soltn
   stopifnot(r %in% c("a", "b", "c"))
   replicate <- ifelse(r == "a", 1,
                       ifelse(r == "b", 2, 3))
@@ -23,21 +24,29 @@ createMetadata <- function(string) {
 readSeqData <- function(file) {
   dat.tmp <- read.delim(as.character(file), sep="", header=F, 
                         stringsAsFactors=F)
-  accn    <- metadata$accn[metadata$path == file]
-  return(data.frame(seq=dat.tmp[,1], copy=dat.tmp[,2], accn=accn))  
+  meta    <- as.list(metadata[metadata$path == file, ])
+  return(data.frame(seq=dat.tmp[,1], copy=dat.tmp[,2], meta))  
 }
 
 
 ## Script body
 ##----------------------------------------------------------------------------
 ## Data importing
-cat("Collecting paths to data...\n")
+cat("Collecting paths to data... ")
 metadata  <- do.call(rbind, lapply(files, createMetadata))
+cat("done.\n")
 cat("Reading sequence data... ")
-data      <- do.call(rbind, lapply(metadata$path, readSeqData))
-data$seq  <- as.character(df$seq)
-cat("done.")
+df <- do.call(rbind, lapply(metadata$path, readSeqData))
+df$seq <- as.character(df$seq)
+cat("done.\n")
 
-## Identifying singletons
-
+## Identify singletons appearing across replicates
+singletons <- df[df$copy == 1, c("seq", "patient")]
+across.replicates <- which(duplicated(singletons))
+n.singletons.replicated <- length(across.replicates)
+n.singletons <- length(singletons$seq)
+proportion.singletons <- n.singletons.replicated/n.singletons
+cat("\n\t", "Total singletons across replicates:", n.singletons.replicated,
+    "\n\t", "Proportion of total singletons:", n.singletons.replicated, "/", 
+    n.singletons, "=", proportion.singletons, "\n\n")
 
