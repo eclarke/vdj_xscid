fb053f7f17d179216c06a78c54002654274e1640
Erik Clarke <ecl@mail.med.upenn.edu>
2013-05-06 16:36:36 -0400

    revamped psl_processing to handle range of thresholds

--- psl_processing.py
+++ psl_processing.py
@@ -1,89 +1,72 @@
-#!/usr/bin/env python
+"""PSL File Processor.
+Description:
+    Aggregates the number matches above two thresholds
 
-import csv
-from argparse import ArgumentParser
-parser = ArgumentParser()
+Usage: 
+	psl_processor.py [options] <input>
 
-parser.add_argument("input", help="psl blat results")
-parser.add_argument("-o", "--out", dest="outfile",
-	help="write results to given filename")
-parser.add_argument("-id1", dest="pct_id_cutoff_1",
-	default=0.99, type=float,
-	help="The percent identity cutoff that determines if the sequence is "+
-		    "'identical' to another. Default 0.99.")
-# parser.add_argument("-al1", dest="align_len_cutoff_1",
-# 	default=59, type=int,
-# 	help="The alignment length cutoff, that, with percent identity 1, " +
-# 	"determines if the sequence is 'identical' to another. Default 59.")
-parser.add_argument("-id2", dest="pct_id_cutoff_2",
-	default=0.9, type=float,
-	help="The percent identity cutoff that determines if the sequence is "+
-		    "'similar' to another. Default 0.9.")
-# parser.add_argument("-al2", dest="align_len_cutoff_2",
-# 	default=54, type=int,
-# 	help="The alignment length cutoff, that, with an percent identity 2, " +
-# 	"determines if the sequence is 'similar' to another. Default 54.")
-parser.add_argument("-range", dest="pct_id_range",
-		    type=float, nargs=3, help="The range of percent identities" +
-		    " to test, in the format 'start' 'stop' 'step'. This " +
-		    "calculates the appropriate alignment length for each out of 60. [NOT YET IMPLEMENTED]")
-		    
-parser.add_argument("-q", action="store_true", help="Be quiet.")
+Options:
+  -h --help                 show this help message and exit
+  --max=MAX                 the maximum % identity threshold [default=0.99]
+  --min=MIN                 the minimum % identity threshold [default=0.85]
+  --step=STEP               spacing of intermediate % id thresholds [default=0.05]
+  -o, --output FILE         specify output file [default input + .out]
+  -q --quiet                operate in quiet mode
 
-args = parser.parse_args()
-outfile = args.outfile if args.outfile else args.input+'.out'
-al1 = int(60 * args.pct_id_cutoff_1)
-al2 = int(60 * args.pct_id_cutoff_2)
-print "your input file is {}".format(args.input)
-print "your out file is {}".format(outfile)
+"""
 
-def frange(args):
-	x, y, step = args
-	while x > y:
-		yield x
-		x -= step
+import csv, sys
+from docopt import docopt
+
+def frange(_min, _max, step):
+    _min = int(_min * 100)
+    _max = int(_max * 100)
+    step = int(step * 100)
+    while _max >= _min:
+        yield _max / 100.0
+        _max -= step
 
 def parserow(row):
-	row = row.split('\t')
-	length = float(row[12]) - float(row[11])
-	pct_id = float(row[0])/length
-	accn_tmp = row[9].split('_')
-	accn, idx = accn_tmp[0], accn_tmp[2]
-	return (accn, idx, pct_id, length)
+    row = row.split('\t')
+    length = float(row[12]) - float(row[11])
+    pct_id = float(row[0]) / length 
+    accn_tmp = row[9].split('_')
+    accn, idx = accn_tmp[0], accn_tmp[2]
+    return (accn, idx, pct_id, length)
 
-with open(outfile, 'w') as out:
-	writer = csv.writer(out, delimiter='\t')
-	if args.pct_id_range:
-		writer.writerow(["accn", "idx"] + [pct for pct in frange(args.pct_id_range)])
-	else:
-		writer.writerow(["accn", "idx", "identical", "similar"])
-	with open(args.input) as psl:
-		oldidx = None
-		oldaccn = None
-		identical = 0
-		similar = 0
-		for i, row in enumerate(psl):
-			accn, idx, pct_id, align_len = parserow(row)
-			if not args.q:
-				print "{0}\r".format(i),
-			if not idx:
-				oldidx = idx
-				oldaccn = accn
-			if idx != oldidx:
-				writer.writerow([oldaccn, oldidx, identical, similar])
-				oldidx = idx
-				oldaccn = accn
-				identical = 0
-				similar = 0
-			if args.pct_id_range:
-				for pct_id_tmp in frange(args.pct_id_range):
-					align_len_tmp = int(60*pct_id_tmp)
-					if ((pct_id >= pct_id_tmp) and
-					    (align_len >= align_len_tmp)):
-						pass
-			if ((pct_id >= args.pct_id_cutoff_1) and 
-				(align_len >= al1)):
-				identical += 1
-			elif ((pct_id >= args.pct_id_cutoff_2) and 
-				(align_len >= al2)):
-				similar += 1
+if __name__ == '__main__':
+    args = docopt(__doc__, version="1.0.0")
+    infile = args["<input>"]
+    outfile = args["--output"] if args["--output"] else '.'.join([infile, "out"])
+    _min = float(args['--min']) if args['--min'] else 0.84
+    _max = float(args['--max']) if args['--max'] else 0.99
+    step = float(args['--step']) if args['--step'] else 0.05
+    thresholds = [(threshold, int(60*threshold)) for threshold 
+                    in frange(_min, _max, step)]
+    print thresholds
+
+    print("Your output file is {}".format(outfile))
+    print(outfile)
+    with open(outfile, 'w') as out:
+        writer = csv.writer(out, delimiter='\t')
+        writer.writerow(["accn", "idx"] + ['id.'+str(int(t[0]*100)) for t in thresholds])
+        with open(infile) as psl:
+            oldidx = None
+            oldaccn = None
+            matches = {threshold[0]:0 for threshold in thresholds}
+            for i, row in enumerate(psl):
+                accn, idx, pct_id, align_len = parserow(row)
+                if not args["-q"]:
+                    print "{0}\r".format(i),
+                if not idx:
+                    oldidx = idx
+                    oldaccn = accn
+                if idx != oldidx:
+                    writer.writerow([oldaccn, oldidx, identical, similar])
+                    oldidx = idx
+                    oldaccn = accn
+                    matches = {threshold[0]:0 for threshold in thresholds}
+                for _id, _al in thresholds:
+                    if (pct_id >= _id and align_len >= _al):
+                        matches[_id] += 1
+                        continue
